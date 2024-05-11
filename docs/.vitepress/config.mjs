100832491ae427a28c7e78a847d8cda397cb5b17
NOIR-DexPrkr <132374149+NOIR-DexPrkr@users.noreply.github.com>
2024-05-11 13:35:34 -0500

    better detailed search

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -48,22 +48,7 @@ export default defineConfig({
     search: {
       provider: 'local',
       options: {
-        miniSearch: {
-          /**
-           * @type {Pick<import('minisearch').Options, 'extractField' | 'tokenize' | 'processTerm'>}
-           */
-          options: {
-            /* ... */
-          },
-          /**
-           * @type {import('minisearch').SearchOptions}
-           * @default
-           * { fuzzy: 0.2, prefix: true, boost: { title: 4, text: 2, titles: 1 } }
-           */
-          searchOptions: {
-            detail: true,
-            /* ... */
-          }}
+        detailedView: true
     }},
     
     sidebar: {
