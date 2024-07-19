ee6a54c5e54407ecbd2254cfc7e485a984a2f566
NOIR-DexPrkr <132374149+NOIR-DexPrkr@users.noreply.github.com>
2024-07-19 01:12:27 -0500

    logo

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -34,7 +34,7 @@ export default defineConfig({
     nav: [
       { text: 'Inicio', link: '/' },
       { text: 'Buscadores', link: '/buscadores', activeMatch: '/buscadores'},
-      { text: '🔁 15 julio 24', link: '/', activeMatch: '/d'}
+      { text: '🔁 19 julio 24', link: '/', activeMatch: '/d'}
     ],
       
     head: [['link', { rel: 'icon', href: 'https://github.com/NOIR-DexPrkr/noiroom/blob/main/docs/favicon.ico' }]],
